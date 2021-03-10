606fd56852a3d63c9ce4511d9ccf70d1b41b3414
Timur Badretdinov <destinerx@gmail.com>
2021-03-11 00:53:25 +0300

    Setup Tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createWebHistory, createRouter } from 'vue-router';
 
 import App from './App.vue';
+import './index.css';
 
 import Main from './pages/Main.vue';
 import About from './pages/About.vue';
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	purge: ['./index.html', './src/**/*.{vue,js,ts}'],
+	darkMode: false,
+	theme: {
+		fontFamily: {
+			'sans': [
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'Roboto',
+				'Helvetica',
+				'Arial',
+				'sans-serif',
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+			],
+		},
+	},
+	plugins: [],
+};
