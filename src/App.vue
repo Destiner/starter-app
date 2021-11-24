b986abadb3c7a311ce5b4758c0c1d701b858971e
Timur Badretdinov <destinerx@gmail.com>
2021-11-24 22:34:27 +0300

    CSS reset

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,53 @@
 
 <style>
 body {
-	margin: 0;
 	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', Helvetica, Arial, sans-serif;
+}
+
+/* Reset */
+
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+}
+
+html,
+body {
+	height: 100%;
+}
+
+body {
+	-webkit-font-smoothing: antialiased;
+}
+
+img,
+picture,
+video,
+canvas,
+svg {
+	display: block;
+	max-width: 100%;
+}
+
+input,
+button,
+textarea,
+select {
+	font: inherit;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+	overflow-wrap: break-word;
+}
+
+#app {
+	isolation: isolate;
 }
 </style>
