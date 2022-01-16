7e1a37a27125dc2e1ca71eef16fb3444517a248a
Timur Badretdinov <destinerx@gmail.com>
2022-01-16 06:38:23 +0300

    Rewrite Main page with script setup

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -6,33 +6,22 @@
 	</div>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent, onMounted } from 'vue';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 
 import Banner from '@/components/Banner.vue';
 
-export default defineComponent({
-	components: {
-		Banner,
-	},
-	setup() {
-		const block = ref('0');
+const block = ref('0');
 
-		async function getLatestBlock(): Promise<string> {
-			const url = 'https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=1578638524&closest=before&apikey=IIMQH3GC81HQYSFTZCW4AKHDFSWKYPV6C1';
-			const data = await fetch(url);
-			const json = await data.json();
-			const block = json.result;
-			return block;
-		}
+async function getLatestBlock(): Promise<string> {
+	const url = 'https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=1578638524&closest=before&apikey=IIMQH3GC81HQYSFTZCW4AKHDFSWKYPV6C1';
+	const data = await fetch(url);
+	const json = await data.json();
+	const block = json.result;
+	return block;
+}
 
-		onMounted(async () => {
-			block.value = await getLatestBlock();
-		});
-
-		return {
-			block,
-		};
-	},
+onMounted(async () => {
+	block.value = await getLatestBlock();
 });
 </script>
