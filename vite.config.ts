f8b208e2a6de2d9a9d3759de72aaf6dd82455be6
Timur Badretdinov <destinerx@gmail.com>
2022-02-11 08:15:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import path from 'path';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [vue()],
@@ -8,8 +9,5 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
 		},
-	},
-	server: {
-		port: 8080,
 	},
 });
