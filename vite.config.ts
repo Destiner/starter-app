2e6912c69739c36da85fef9c48b2c277b2a77bb7
Timur Badretdinov <accounts@destiner.io>
2024-03-08 11:36:16 +0000

    Use Vue dev tools Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), vue()],
+  plugins: [tsconfigPaths(), vue(), VueDevTools()],
 });
